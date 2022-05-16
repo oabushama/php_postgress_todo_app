44e8ae76cf8f37c45a50529832bd552272b27c06
Khalid Alharbi <khalid3ahmed@gmail.com>
2022-05-16 18:35:19 +0000

    Update delete_todo.php

--- delete_todo.php
+++ delete_todo.php
@@ -3,7 +3,7 @@
 function delete_item($checkBoxList){
     foreach($checkBoxList as $value) {
         // create a prepared update statement
-        $delete_statement = $GLOBALS['conn']->prepare("DELETE FROM apps.tasks WHERE id = ?");
+        $delete_statement = $GLOBALS['conn']->prepare("DELETE FROM tasks WHERE id = ?");
         if($delete_statement) {
             $delete_statement->bindParam(1, $value, PDO::PARAM_INT);
             if (!$delete_statement->execute()) {
